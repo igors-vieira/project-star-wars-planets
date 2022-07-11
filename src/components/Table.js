e5ac4fceebb1f0336545d9155fe446a1337afb92
Igor Vieira <igorsv2240@gmail.com>
2022-07-11 13:55:55 -0300

    requisito 3

--- src/components/Table.js
+++ src/components/Table.js
@@ -4,14 +4,45 @@ import StarContext from '../context/StarContext';
 function Table() {
   const { planets } = useContext(StarContext);
   const [filterByName, setFilterByName] = useState({ name: '' });
+  const [filterByNumericValues,
+    setFilterByNumericValues] = useState([]);
 
-  const handleFilter = ({ target }) => {
+  const handleName = ({ target }) => {
     setFilterByName({ name: target.value });
   };
 
+  const handleFilter = (e) => {
+    const { target } = e;
+    e.preventDefault();
+    setFilterByNumericValues({
+      column: target[1].value,
+      comparison: target[2].value,
+      value: Number(target[3].value),
+    });
+    console.log(filterByNumericValues);
+  };
+
   return (
-    <div className="App">
-      <input className="App" data-testid="name-filter" onChange={ handleFilter } />
+    <div>
+      <form onSubmit={ handleFilter }>
+        <input type="text" data-testid="name-filter" onChange={ handleName } />
+        <select data-testid="column-filter">
+          <option value="population">population</option>
+          <option value="diameter">diameter</option>
+          <option value="orbital_period">orbital_period</option>
+          <option value="rotation_period">rotation_period</option>
+          <option value="surface_water">surface_water</option>
+        </select>
+        <select data-testid="comparison-filter">
+          <option value="maior que">maior que</option>
+          <option value="menor que">menor que</option>
+          <option value="igual a">igual a</option>
+        </select>
+        <input type="number" defaultValue="0" data-testid="value-filter" />
+        <button type="submit" data-testid="button-filter">
+          Adicionar Filtro
+        </button>
+      </form>
       <table>
         <thead>
           <tr>
@@ -32,7 +63,23 @@ function Table() {
         </thead>
         <tbody>
           {planets && planets
-            .filter(({ name }) => name.includes(filterByName.name)).map((planet, i) => (
+            .filter(({ name }) => name.includes(filterByName.name))
+            .filter((planet) => {
+              if (filterByNumericValues.comparison === 'maior que') {
+                return planet[filterByNumericValues
+                  .column] > filterByNumericValues.value;
+              }
+              if (filterByNumericValues.comparison === 'menor que') {
+                return planet[filterByNumericValues
+                  .column] < filterByNumericValues.value;
+              }
+              if (filterByNumericValues.comparison === 'igual a') {
+                return Number(planet[filterByNumericValues
+                  .column]) === filterByNumericValues.value;
+              }
+              return true;
+            })
+            .map((planet, i) => (
               <tr key={ i + planet.name }>
                 <th scope="row">{planet.name}</th>
                 <td>{planet.rotation_period}</td>
